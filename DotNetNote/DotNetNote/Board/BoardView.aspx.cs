a0a4bfa528caa209eaa8bfb98ef572e2c042e829
JaehyeonHeo <jhheo06@gmail.com>
2021-04-15 17:25:42 +0900

    2021 04 15 DotNetNote 커밋

--- DotNetNote/DotNetNote/Board/BoardView.aspx.cs
+++ DotNetNote/DotNetNote/Board/BoardView.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,7 +15,45 @@ namespace DotNetNote.Board
         
         protected void Page_Load(object sender, EventArgs e)
         {
+            lnkDelete.NavigateUrl = $"BoardDelete.aspx?Id={Request["Id"]}";
+            lnkModify.NavigateUrl = $"BoardModify.aspx?Id={Request["Id"]}"; 
+            lnkReply.NavigateUrl = $"BoardReply.aspx?Id={Request["Id"]}";
 
+            _Id = Request["Id"];
+            if (_Id == null) Response.Redirect("BoardList.aspx");
+            if (!Page.IsPostBack) DisplayData(); 
+        }
+
+        private void DisplayData()
+        {
+            var repo = new DbRepository();
+            Note note = repo.GetNoteById(Convert.ToInt32(_Id));
+
+            lblNum.Text = _Id;
+            lblName.Text = note.Name;
+            lblEmail.Text = string.Format("<a href='mailto:{0}'>{0}</a>", note.Email);
+            lblTitle.Text = note.Title;
+
+            string content = note.Content;
+            // 인코딩 방식 : Text, Html, Mixed
+            string encoding = note.Encoding; 
+            if (encoding == "Text")
+            {
+                lblContent.Text = Helpers.HtmlUtility.EncodeWithTabAndSpace(content); 
+            }
+            else if (encoding == "Mixed")
+            {
+                lblContent.Text = content.Replace("\r\n", "<br />"); 
+            }
+            else
+            {
+                lblContent.Text = content; // HTML은 변환 없음 !
+            }
+
+            lblReadCount.Text = note.ReadCount.ToString();
+            lblHomepage.Text = $"<a href='{note.Homepage}' target='_blank'>{note.Homepage}</a>";
+            lblPostDate.Text = note.PostDate.ToString();
+            lblPostIP.Text = note.PostIp; 
         }
     }
 }
