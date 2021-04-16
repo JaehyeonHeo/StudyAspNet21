d969cf2ca0ea0403174eb0c24bd7c45c47e60178
JaehyeonHeo <jhheo06@gmail.com>
2021-04-16 16:16:06 +0900

    DotNetNote 최종

---
+++ DotNetNote/DotNetNote/Board/BoardDown.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BoardDown.aspx.cs" Inherits="DotNetNote.Board.BoardDown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--다운로드화면(아무것도 없음)--%>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DotNetNote/DotNetNote/Controls/CommentControl.ascx.cs
@@ -0,0 +1,39 @@
+﻿using DotNetNote.Models;
+using System;
+using System.Web.UI;
+
+namespace DotNetNote.Controls
+{
+    public partial class CommentControl : System.Web.UI.UserControl
+    {
+        private DbRepository _repo;
+
+        public CommentControl()
+        {
+            _repo = new DbRepository(); 
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ctlCommentList.DataSource = _repo.GetNoteComments(Convert.ToInt32(Request["Id"]));
+                ctlCommentList.DataBind(); 
+            }
+        }
+
+        protected void btnWriteComment_Click1(object sender, EventArgs e)
+        {
+            NoteComment comment = new NoteComment();
+            comment.BoardId = Convert.ToInt32(Request["Id"]);  // 게시글 번호 
+            comment.Name = txtName.Text;
+            comment.Password = txtPassword.Text;
+            comment.Opinion = txtOpinion.Text; // 댓글 내용 
+
+            _repo.AddNoteComment(comment); //DB 저장 
+
+            Response.Redirect($"{Request.ServerVariables["SCRIPT_NAME"]}?Id={Request["Id"]}"); // Boardview.aspx 동적으로 웹페이지 이름 할당
+
+        }
+    }
+}
