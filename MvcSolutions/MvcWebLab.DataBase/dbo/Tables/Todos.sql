0b1f39f11958f90a0107632b0b070a9a9d6a412f
JaehyeonHeo <jhheo06@gmail.com>
2021-04-19 17:03:58 +0900

    20210419 Commit

---
+++ MvcSolutions/MvcWebLab.DataBase/dbo/Tables/Todos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Todos]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(150) NULL, 
+    [IsDone] BIT NULL, 
+    [CreationDate] DATETIME NULL DEFAULT (GetDate())
+)
---
+++ MvcSolutions/TodoListWebApp/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListWebApp.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "제목을 입력하세요 !")]
+        public string Title { get; set; }   
+        public bool IsDone { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
