1b161382233b85fe6ccf56d38ca4e3167d731ee3
JaehyeonHeo <jhheo06@gmail.com>
2021-04-21 17:19:25 +0900

    20210421 Commit

---
+++ MvcSolutions/MyPortpolio/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System; 
+
+namespace MyPortfolio.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email을 입력하세요")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password를 입력하세요")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Grade { get; set; }
+
+    }
+}
---
+++ MvcSolutions/MyPortpolio/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolio.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="이름을 입력하세요")]
+        [DataType(DataType.Text)]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "이메일을 입력하세요")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "내용을 입력하세요")]
+        [DataType(DataType.Text)]
+        public string Contents { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime RegDate { get; set; }
+    }
+}
